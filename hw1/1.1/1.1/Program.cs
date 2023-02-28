bef071e3a0fec80935318cc99289b56dce9c83c4
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-02-28 20:43:05 +0300

    task 1.1

---
+++ hw1/1.1/1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the size of the array:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[length];
+            Console.WriteLine("enter the elements of the array:");
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+            
+            Console.WriteLine("sorted array is:");
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
