ea4de4e7f7c9e517c7adb597c5a9866890b2d0ca
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-07-20 06:31:06 +0300

    implement necessary classes

---
+++ hw4/task1/task1/AST.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace task1
+{
+    internal class AST
+    {
+        private IASTNode root;
+
+        private IASTNode BuildTree(string expression, ref int pos)
+        {
+            IASTNode node;
+            for (pos = 0; pos < expression.Length; pos++)
+            {
+                switch(expression[i])
+                {
+                    case ' ':
+                        continue;
+                    case '(':
+                        char op;
+                        IASTNode leftOperand = null;
+                        IASTNode rightOperand = null;
+                        for (; pos < expression.Length; pos++)
+                        {
+                            if (expression[pos] == ' ')
+                                continue;
+                            if ("+-*/".Contains(expression[pos]))
+                                op = expression[pos];
+                            else
+                                throw new InvalidExpressionException("invalid expression");
+                            leftOperand = BuildTree(expression, ref pos);
+                            rightOperand = BuildTree(expression, ref pos);
+                            node = new Operator(op, leftOperand, rightOperand);
+                        }
+                        break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        int numberLength = pos + 1;
+                        while (numberLength < expression.Length && char.IsDigit(expression[pos]))
+                        break;
+                    case ')':
+                        return node;
+
+                }
+            }
+        }
+        public AST(string expression)
+        {
+            root = BuildTree(expression, 0);
+        }
+    }
+}
---
+++ hw4/task1/task1/Operator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Operator : IASTNode
+    {
+        private readonly string ALLOWED_OPERATORS = "+-*/";
+        public Operator(char op)
+        {
+            if (!ALLOWED_OPERATORS.Contains(op))
+                throw new Exception("incorret operator");
+            this.op = op;
+        }
+        public double Evaluate()
+        {
+
+        }
+    }
+}
