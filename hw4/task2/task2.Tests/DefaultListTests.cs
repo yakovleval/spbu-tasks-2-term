94838a9930d9985db5c3ad7fe168fb03fc127ee2
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-07-25 00:36:12 +0300

    add separate tests for DefaultList and UniqueList

---
+++ hw4/task2/task2.Tests/DefaultListTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2.Tests
+{
+    public class DefaultListTests
+    {
+        private DefaultList list;
+        [SetUp]
+        public void SetUp() => list = new DefaultList();
+        [Test]
+        public void TestAddExistingElement()
+        {
+            list.Add(1);
+            list.Add(1);
+        }
+
+        [Test]
+        public void TestRemoveFromEmptyList()
+        {
+            Assert.IsFalse(list.Remove(1));
+        }
+        [Test]
+        public void TestRemoveNonExistentElement()
+        {
+            list.Add(1);
+            list.Add(2);
+            Assert.IsFalse(list.Remove(3));
+        }
+    }
+}
---
+++ hw4/task2/task2.Tests/UniqueListExceptionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2.Tests
+{
+    public class UniqueListExceptionsTests
+    {
+        private UniqueList list;
+        [SetUp]
+        public void SetUp() => list = new UniqueList();
+        [Test]
+        public void TestAddExistingElement()
+        {
+            list.Add(1);
+            Assert.Throws<AdditionOfExistentElementException>(() => list.Add(1));
+        }
+        [Test]
+        public void TestRemoveFromEmptyList()
+        {
+            Assert.Throws<DeletingNonExistingElementException>(() => list.Remove(1));
+        }
+        [Test]
+        public void TestRemoveNonExistentElement()
+        {
+            list.Add(1);
+            list.Add(2);
+            Assert.Throws<DeletingNonExistingElementException>(() => list.Remove(3));
+        }
+    }
+}
