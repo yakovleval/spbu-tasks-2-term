f5b62a238fdc23aa11221968e09b30a2d922bf23
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-07-26 02:32:07 +0300

    implement console app (without exceptions yet)

---
+++ hw5/task1/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Edge
+    {
+        public int Vertex1 { get; }
+        public int Vertex2 { get; }
+        public int Weight { get; }
+        public Edge(int vertex1, int vertex2, int weight)
+        {
+            Vertex1 = vertex1;
+            Vertex2 = vertex2;
+            Weight = weight;
+        }
+    }
+}
---
+++ hw5/task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using task1.Exceptions;
+
+namespace task1
+{
+    internal class Program
+    {
+        private static List<Edge> ReadGraph(string path)
+        {
+            string pattern = @"^(\d+): ?(\d+ ?\(\d+\),? ?)+$";
+            var lines = File.ReadAllLines(path);
+            List<Edge> graph = new();
+            foreach (var line in lines)
+            {
+                if (!Regex.IsMatch(line, pattern))
+                    throw new PatternMismatchException();
+                var integers = Regex.Split(line, @"\D+").ToList().ConvertAll(int.Parse);
+                int vertex = integers[0];
+                for (int i = 1; i < integers.Count; i += 2)
+                {
+                    int neighbour = integers[i];
+                    int weight = integers[i + 1];
+                    graph.Add(new Edge(vertex, neighbour, weight));
+                }
+            }
+            return graph;
+        }
+        private static void WriteGraph(List<Edge> graph, string path)
+        {
+            Dictionary<int, List<(int, int)>> listsOfNeighbours = new();
+            foreach (var edge in graph)
+            {
+                listsOfNeighbours[edge.Vertex1].Add((edge.Vertex2, edge.Weight));
+            }
+            foreach (var (vertex, list) in listsOfNeighbours)
+            {
+
+                string stringifiedList = string.Join(", ", list
+                    .Select((neighbour, weight) => $"{neighbour} ({weight})")
+                    .ToArray<string>());
+                Console.WriteLine($"{vertex}: {stringifiedList}");
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    Console.WriteLine("""
+                        usage: dotnet run [<path to input file>] [<path to output file>]
+                        """);
+                    return 0;
+                }
+                string inputPath = args[0];
+                string outputPath = args[1];
+
+                List<Edge> graph = ReadGraph(inputPath);
+                var MSTtree = MST.BuildMST(graph);
+                WriteGraph(MSTtree, outputPath);
+            }
+
+
+        }
+    }
+}
