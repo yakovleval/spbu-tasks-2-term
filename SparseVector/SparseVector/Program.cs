742422122a2a12cce0cc25884e89f216673d6bea
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-09-04 13:16:25 +0300

    SparseVectors with tests and comments

---
+++ SparseVector/SparseVector/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SparseVector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> kek = new();
+            Console.WriteLine(kek[0]);
+        }
+    }
+}
