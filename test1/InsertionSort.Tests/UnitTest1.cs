7591e1dd0dcddca32ef737a53cb9084da15fa022
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-09-04 11:25:46 +0300

    test1

---
+++ test1/InsertionSort.Tests/UnitTest1.cs
@@ -0,0 +1,65 @@
+using System.ComponentModel;
+using System.Linq;
+
+namespace InsertionSort.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private bool AreArraysEqual<T>(T[] array1, T[] array2) where T : IComparable
+        {
+            if (array1.Length != array2.Length)
+                return false;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].CompareTo(array2[i]) != 0)
+                    return false;
+            }
+            return true;
+        }
+
+        class ReverseOrderComparer<T> : IComparer<T> where T : IComparable
+        {
+            public int Compare(T x, T y) => x.CompareTo(y) * -1;
+        }
+
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 4, 3, 2, 1 }, new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] { 2, 1, 1, 1 }, new int[] { 1, 1, 1, 2 })]
+        public void TestIntArrayWithDefaultAndReverseOrderComparer(int[] arrayToSort, int[] expectedArray)
+        {
+            List<int> resultList = InsertionSort.Sort(arrayToSort.ToList<int>(), Comparer<int>.Default);
+            int[] resultArray = resultList.ToArray();
+            Assert.That(AreArraysEqual(resultArray, expectedArray), Is.True);
+
+            IComparer<int> reverseOrder = new ReverseOrderComparer<int>();
+            List<int> resultListReverseOrder = InsertionSort.Sort(arrayToSort.ToList<int>(), reverseOrder);
+            resultArray = resultListReverseOrder.ToArray();
+            Array.Reverse(expectedArray);
+            Assert.That(AreArraysEqual(resultArray, expectedArray), Is.True);
+        }
+
+        [TestCase(new string[] { }, new string[] { })]
+        [TestCase(new string[] { "abcd" }, new string[] { "abcd" })]
+        [TestCase(new string[] { "d", "c", "b", "a" }, new string[] { "a", "b", "c", "d" })]
+        [TestCase(new string[] { "xyz", "bar", "foo" }, new string[] { "bar", "foo", "xyz" })]
+        public void TestStringArrayWithDefaultAndReverseOrderComparer(string[] arrayToSort, string[] expectedArray)
+        {
+            List<string> resultList = InsertionSort.Sort(arrayToSort.ToList<string>(), Comparer<string>.Default);
+            string[] resultArray = resultList.ToArray();
+            Assert.That(AreArraysEqual(resultArray, expectedArray), Is.True);
+
+            IComparer<string> reverseOrder = new ReverseOrderComparer<string>();
+            List<string> resultListReverseOrder = InsertionSort.Sort(arrayToSort.ToList<string>(), reverseOrder);
+            resultArray = resultListReverseOrder.ToArray();
+            Array.Reverse(expectedArray);
+            Assert.That(AreArraysEqual(resultArray, expectedArray), Is.True);
+        }
+    }
+}
---
+++ test1/InsertionSort/InsertionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    /// <summary>
+    /// Class that implements insertion sort algorithm
+    /// </summary>
+    public class InsertionSort
+    {
+        /// <summary>
+        /// sorts a list of T values with given comparer
+        /// </summary>
+        /// <typeparam name="T">type of elements to sort</typeparam>
+        /// <param name="listToSort">list of values of type T to sort</param>
+        /// <param name="comparer">comparer object to compare values of type T</param>
+        /// <returns></returns>
+        public static List<T> Sort<T>(List<T> listToSort, IComparer<T> comparer)
+        {
+            int[] kek = new int[] { };
+            for (int i = 1; i < listToSort.Count; i++)
+            {
+                int j = i;
+                while (j > 0 && comparer.Compare(listToSort[j - 1], listToSort[j]) > 0)
+                {
+                    T tmp = listToSort[j - 1];
+                    listToSort[j - 1] = listToSort[j];
+                    listToSort[j] = tmp;
+                    j--;
+                }
+            }
+            return listToSort;
+        }
+    }
+}
