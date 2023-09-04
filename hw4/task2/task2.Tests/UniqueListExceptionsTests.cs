9e517e1b440a8fa0ed7c291afb9071be50f9cce2
YakovlevAl <sanya89_muravlenko@mail.ru>
2023-09-04 15:18:03 +0300

    run code cleaner

--- hw4/task2/task2.Tests/UniqueListExceptionsTests.cs
+++ hw4/task2/task2.Tests/UniqueListExceptionsTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace task2.Tests
+﻿namespace task2.Tests
 {
     public class UniqueListExceptionsTests
     {
--- hw4/task2/task2/UniqueList.cs
+++ hw4/task2/task2/UniqueList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace task2
+﻿namespace task2
 {
     /// <summary>
     /// extends List class: stores only unique elements
